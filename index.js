fb87de98a10d9b97a5549e7dc1e9929847e4ff86
aaklesh99 <aaklesh99@gmail.com>
2022-07-28 09:04:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+
+   var button1=document.getElementById('btn1');
+   var button2=document.getElementById('btn2');
+   var button3=document.getElementById('btn3');
+   var button4=document.getElementById('btn4');
+   function changeColor()   
+     {  
+          document.getElementById('btn1').style.backgroundColor = 'rgb(32, 74, 60)'; 
+          document.getElementById('btn2').style.backgroundColor = 'rgb(72, 96, 107)'; 
+          document.getElementById('btn3').style.backgroundColor = 'rgb(72, 96, 107)'; 
+          document.getElementById('btn4').style.backgroundColor = 'rgb(72, 96, 107)';  
+            
+     }  
+     function changeColor1()   
+     {  
+          document.getElementById('btn2').style.backgroundColor = 'rgb(32, 74, 60)';  
+          document.getElementById('btn1').style.backgroundColor = 'rgb(72, 96, 107)'; 
+          document.getElementById('btn3').style.backgroundColor = 'rgb(72, 96, 107)'; 
+          document.getElementById('btn4').style.backgroundColor = 'rgb(72, 96, 107)'; 
+            
+     }  
+     function changeColor2()   
+     {  
+          document.getElementById('btn3').style.backgroundColor = 'rgb(32, 74, 60)';
+          document.getElementById('btn1').style.backgroundColor = 'rgb(72, 96, 107)';
+          document.getElementById('btn2').style.backgroundColor = 'rgb(72, 96, 107)';  
+          document.getElementById('btn4').style.backgroundColor = 'rgb(72, 96, 107)';
+            
+     }  
+     function changeColor3()   
+     {  
+          document.getElementById('btn4').style.backgroundColor = 'rgb(32, 74, 60)';  
+          document.getElementById('btn1').style.backgroundColor = 'rgb(72, 96, 107)';
+          document.getElementById('btn2').style.backgroundColor = 'rgb(72, 96, 107)';
+          document.getElementById('btn3').style.backgroundColor = 'rgb(72, 96, 107)';            
+            
+     }  
+
+
+     function english()
+     {
+          location.href="english.html";
+     }
+     function math()
+     {
+          location.href="math.html";
+     }
+     function reasoning()
+     {
+          location.href="reansoning.html";
+     }
+     function goToSelect()
+     {
+          location.href="quiz_select.html";
+          
+     }
+     function onclick()
+     {
+          location.href="index.html";
+     }
+
+     
+     
